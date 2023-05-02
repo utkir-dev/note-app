17575db3112853595fb0ab87a0de324b08cf902e
U <umtokh80@gmail.com>
2023-05-02 11:39:37 +0500

    initial

---
+++ app/src/main/java/com/example/mynotes/domain/use_cases/auth_use_case/CheckUserUseCase.kt
@@ -0,0 +1,11 @@
+package com.example.mynotes.domain.use_cases.auth_use_case
+
+import com.example.mynotes.data.repositories.AuthRepository
+import javax.inject.Inject
+
+class CheckUserUseCase @Inject constructor(
+    private val repository: AuthRepository
+) {
+    suspend operator fun invoke() = repository.currentUser
+
+}
---
+++ app/src/main/java/com/example/mynotes/presentation/utils/components/edittext/MyEditText.kt
@@ -0,0 +1,9 @@
+package com.example.mynotes.presentation.utils.components.edittext
+
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyEditText() {
+
+}
